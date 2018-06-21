74441e62994a269b28e178aaf934eedd8f61cc07
Abby Lortz <abilenelortz03@gmail.com>
2018-06-21 16:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 function tailsNeverFails() {
   let tails = 0;
   
-  while (Math.random() >= 0.01)
+  while (Math.random() >= 0.01) {
   tails++
 }
 
-  console.log(`You go ${tails} tails in a row!`);
+  console.log(`You got ${tails} tails in a row!`);
 }
 
 tailsNeverFails();
